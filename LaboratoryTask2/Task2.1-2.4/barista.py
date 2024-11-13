a6e425df380e515654c71de21e7cd3aef6c87634
Madalina Chirpicinic <madalina.chirpicinic@gmail.com>
2024-11-13 13:45:43 +0200

    fixed name params passing

--- LaboratoryTask2/Task2.1-2.4/barista.py
+++ LaboratoryTask2/Task2.1-2.4/barista.py
@@ -9,19 +9,19 @@ class Barista:
         for order in orders:
             match order['type']:
                 case 1:
-                    americano = Americano(order['intensity'], order['ml_of_water'], "Americano")
+                    americano = Americano(order['intensity'], order['ml_of_water'], order['name'])
                     americano.makeAmericano()
                     coffee_list.append(americano)
                 case 2:
-                    cappuccino = Cappuccino(order['intensity'], order['ml_of_milk'], "Cappuccino")
+                    cappuccino = Cappuccino(order['intensity'], order['ml_of_milk'], order['name'])
                     cappuccino.makeCappuccino()
                     coffee_list.append(cappuccino)
                 case 3:
-                    pumpkin_spice_latte = PumpkinSpiceLatte(order['intensity'], order['ml_of_milk'], order['mg_of_pumpkin_spice'], "PumpkinSpiceLatte")
+                    pumpkin_spice_latte = PumpkinSpiceLatte(order['intensity'], order['ml_of_milk'], order['mg_of_pumpkin_spice'], order['name'])
                     pumpkin_spice_latte.makePumpkinSpiceLatte()
                     coffee_list.append(pumpkin_spice_latte)
                 case 4:
-                    syrup_cappuccino = SyrupCappuccino(order['intensity'], order['ml_of_milk'], order['syrup'], "SyrupCappuccino")
+                    syrup_cappuccino = SyrupCappuccino(order['intensity'], order['ml_of_milk'], order['syrup'], order['name'])
                     syrup_cappuccino.makeSyrupCappuccino()
                     coffee_list.append(syrup_cappuccino)
         
